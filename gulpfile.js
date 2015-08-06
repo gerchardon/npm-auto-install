a24a129f4756236ef33ee14920968eabc81f8558
Gérôme Chardon <gerome.chardon@gmail.com>
2015-08-07 00:19:35 +0200

    add istanbul and coveralls

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var coveralls = require('gulp-coveralls');
+var istanbul = require('gulp-istanbul');
 
 gulp.task('default', ['test']);
 
@@ -7,6 +9,24 @@ gulp.task('test', function(){
   var reporter = 'spec';
   return gulp.src('test/*.js', {read: false})
     .pipe(mocha({reporter: reporter, growl: true}));
+});
+
+gulp.task('coverage', function(cb){
+  gulp.src('lib/**/*.js')
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .on('finish', function () {
+      gulp.src(['test/*.js'])
+        .pipe(mocha())
+        .pipe(istanbul.writeReports()) // Creating the reports after tests runned
+        .pipe(istanbul.enforceThresholds({ thresholds: { global: 80 } })) // Enforce a coverage of at least 90%
+        .on('end', cb);
+    });
+});
+
+gulp.task('coveralls', ['coverage'], function(){
+  gulp.src('coverage/lcov.info')
+    .pipe(coveralls());
 });
 
 gulp.task('watch', function(){
